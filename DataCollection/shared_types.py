1c0af9ec1b97d8dd27c1d84e06d822d6c9b868d3
caiismyname <davidcai2012@gmail.com>
2018-09-18 23:33:40 -0500

    Working through bugs in Newspaper integration

--- DataCollection/shared_types.py
+++ DataCollection/shared_types.py
@@ -10,18 +10,27 @@ class ArticleData:
         self.body_text = body_text
         self.outlet = outlet
 
-    def to_json(self):
-        jsonOutput = {}
-        jsonOutput["url"] = self.url
-        jsonOutput["headline"] = self.headline
-        jsonOutput["coverageDate"] = self.publication_date
-        jsonOutput["authors"] = self.authors
-        jsonOutput["mediaOutletName"] = self.outlet.name
-        jsonOutput["mediaType"] = self.outlet.media_type
-        jsonOutput["category"] = self.outlet.category
-        jsonOutput["locale"] = self.outlet.locale
 
-        return json.dumps(jsonOutput)
+    def to_json(self):
+        """
+        Exports metadata in a flat JSON structure, as a string. 
+        Use json.loads() to inflate the output of this function into an accessbile JSON object.
+        """
+        json_output = {}
+        json_output["url"] = self.url
+        json_output["headline"] = self.headline if self.headline is not None else "unknown"
+        json_output["coverageDate"] = self.publication_date.strftime("%Y-%m-%d") if self.publication_date is not None else "unknown"
+        json_output["authors"] = self.authors if self.authors is not None else "unknown"
+        json_output["mediaOutletName"] = self.outlet.name if self.outlet.name else "unknown"
+        json_output["mediaType"] = self.outlet.media_type if self.outlet.media_type  is not None else "unknown"
+        json_output["category"] = self.outlet.category if self.outlet.category is not None else "unknown"
+        json_output["locale"] = self.outlet.locale if self.outlet.locale is not None else "unknown"
+
+        return json.dumps(json_output)
+
+    def __str__(self):
+        return json.dumps(json.loads(self.to_json()), sort_keys=True, indent=4)
+
 
 
 class Outlet:
