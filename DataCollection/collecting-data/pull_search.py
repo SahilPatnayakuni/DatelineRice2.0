a87fe7837ed26cc089e5b3039135bfbbbdcffb2d
Shrinithi Narayanan <nithinara@gmail.com>
2018-09-19 00:44:14 -0500

    pushing changes to pull search that create the proper objects and puts them all in a list for further processing in the data collection pipeline

--- DataCollection/collecting-data/pull_search.py
+++ DataCollection/collecting-data/pull_search.py
@@ -1,116 +1,152 @@
 from bs4 import BeautifulSoup
 from newspaper import Article
 import requests, sys, webhoseio
+import sys
 
-from search_rss_shared import *
+sys.path.append("..")
+from shared_types import *
+sys.path.pop()
+
+return_objs = []
 
 current_batch_objs = list()
+search_terms = ['rice university']
 
-def pull_from_search():
-	webhose_newsriver_urls = set()
+for term in search_terms:
+	# make response diff based on search terms
 
 	response = requests.get("https://api.newsriver.io/v2/search?query=text%3A%22rice%20university%22%20OR%20title%3A%22rice%20university%22&sortBy=discoverDate&sortOrder=DESC&limit=15", headers={"Authorization":"sBBqsGXiYgF0Db5OV5tAw-SV99gJE88m8wacCchf2-RRrEiOATHNTquKXyn2goTIn2pHZrSf1gT2PUujH1YaQA"})
 	jsonFile = response.json()
-	all_newsriver_urls = set()
-
 	for resp in jsonFile:
-	    webhose_newsriver_urls.add(resp['url'])
+		url = resp['url']
+		title = None
+		highlight = None
+		publishDate = None
+		siteName = None
+		domainName = None
+		text = None
 
-	# webhoseio.config(token="25ad10f0-486b-408b-a8d2-0709e56808ce")
-	# query_params = {
-	#     "q": "\"rice university\"",
-	#     "ts": "1536006194344",
-	#     "sort": "published"
-	# }
-
-	# output = webhoseio.query("filterWebContent", query_params)
-	# total_results = output['totalResults']
-	# counter = int(1 + total_results / 100)
-
-	# for i in range(counter):
-	#     for j in range(100):
-	#         try:
-	#             url = output['posts'][j]['thread']['url']
-	#             webhose_newsriver_urls.add(url)
-
-	#             try:
-	#                 author = output['posts'][i]['thread']['author']
-	#             except Exception as e:
-	#                 author = None
-
-	#             published = output['posts'][i]['thread']['published']
-	#             title = output['posts'][i]['thread']['title']
-
-	#         except Exception as e:
-	#             break
-	#     output = webhoseio.get_next()
-		
-	for link in webhose_newsriver_urls:
-		source = requests.get(link, timeout=5)
+		source = requests.get(url, timeout=5)
 		BS = BeautifulSoup(source.content, "html.parser")
-		print("LINK: " + link + "\n")
-		print('\tURL: ' + source.url + "\n")
-		
-		try:
-			print ('\t' + BS.find('title').text + "\n")
-		except Exception as e:
-			print("Could not find title")
-
 		article = Article(source.url)
 		article.download()
-
-		if len(sys.argv) > 1 and sys.argv[1] == "--content":
-			print(article.text)
-
 		article.parse()
-		print("\tAUTHORS: " + str(article.authors) + "\n")
-		print("\tPUBLISH DATE: " + str(article.publish_date) + "\n")
-		article.nlp()
-		print("\tKEYWORDS: " + str(article.keywords) + "\n")
-		print("\tSUMMARY: " + str(article.summary) + "\n")
 
-def pull_from_feeds():
-	pass
+		try:
+			title = resp['title']
+		except Exception as e:
+			pass
 
-def extract_metadata(obj):
-	pass
+		try:
+			highlight = resp['highlight']
+		except Exception as e:
+			pass
 
-def is_uniq_article(obj):
-	pass
+		try:
+			publishDate = resp['publishDate']
+		except Exception as e:
+			pass
 
-def is_valid_article(obj):
-	return True
+		try:
+			siteName = resp['website']['name']
+		except Exception as e:
+			pass
 
-def lookup_person():
-	pass
+		try:
+			domainName = resp['website']['domainName']
+		except Exception as e:
+			pass
 
-def send_data():
-	pass
+		try:
+			text = resp['text']
+		except Exception as e:
+			pass
 
-if __name__ == "__main__":
-	print("test")
+		article_obj = ArticleData(url, title, highlight, publishDate, article.authors, Outlet(siteName, None, None, None, domainName), text)
+		print("newsriver" + str(article_obj.to_json()))
+		return_objs.append(article_obj)
 
-	testArticle = Article("asdfadf")
-	testArticle.set_url("o")
-	print(testArticle.get_url())
+	webhoseio.config(token="25ad10f0-486b-408b-a8d2-0709e56808ce")
+	query_params = {
+	    "q": "\"rice university\"", # make this different based on search terms
+	    "ts": "1536006194344",
+	    "sort": "published"
+	}
 
-	# search_objs = pull_from_search()
-	# feed_objs= pull_from_feeds()
+	output = webhoseio.query("filterWebContent", query_params)
+	total_results = output['totalResults']
+	counter = int(1 + total_results / 100)
 
-	# for obj in search_objs:
-	# 	extract_metadata(obj)
-	# 	if is_uniq_article(obj) and is_valid_article(obj):
-	# 		# get people, look them up
-	# 		# add obj to current batch
-	# 		pass
+	for i in range(counter):
+	    for j in range(100):
+	        try:
+	            url = output['posts'][j]['thread']['url']
+	            authors = None
+	            title = None
+	            highlight = None
+	            publishDate = None
+	            siteName = None
+	            domainName = None
+	            text = None
 
-	# for obj in feed_objs:
-	# 	extract_metadata(obj)
-	# 	if is_uniq_article(obj) and is_valid_article(obj):
-	# 		# get people, look them up
-	# 		# add obj to current batch
-	# 		pass
+	            try:
+	            	authors = output['posts'][j]['author']
+	            except Exception as e:
+	            	pass
 
-	# # send the data in the current batch to storage
+	            try:
+	            	title = output['posts'][j]['title']
+	            except Exception as e:
+	            	pass
 
-	# print("FIN")
+	            try:
+	            	highlight = output['posts'][j]['url']
+	            except Exception as e:
+	            	pass
+
+	            try:
+	            	publishDate = output['posts'][j]['published']
+	            except Exception as e:
+	            	pass
+
+	            try:
+	            	domainName = output['posts'][j]['thread']['site']
+	            except Exception as e:
+	            	pass
+
+	            try:
+	            	text = output['posts'][j]['text']
+	            except Exception as e:
+	            	pass
+
+	            if authors == None:
+	            	source = requests.get(url, timeout=5)
+	            	BS = BeautifulSoup(source.content, "html.parser")
+	            	article = Article(source.url)
+	            	article.download()
+	            	article.parse()
+	            	authors = article.authors
+
+	            article_obj = ArticleData(url, title, highlight, publishDate, authors, Outlet(None, None, None, None, domainName), text)
+	            print("webhose" + str(article_obj.to_json()))
+	            return_objs.append(article_obj)
+
+	        except Exception as e:
+	        	break
+
+	        output = webhoseio.get_next()
+
+print(return_objs)
+
+# article = Article(source.url)
+# article.download()
+# article.parse()
+# article.nlp()
+
+# print(article.text)
+# print("LINK: " + link + "\n")
+# print("URL: " + source.url + "\n")
+# print("AUTHORS: " + str(article.authors) + "\n")
+# print("PUBLISH DATE: " + str(article.publish_date) + "\n")
+# print("KEYWORDS: " + str(article.keywords) + "\n")
+# print("SUMMARY: " + str(article.summary) + "\n")
