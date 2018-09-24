bb89b8377c3bce307b2a316161124109576d9970
Lucy Fox <lucy.r.fox@gmail.com>
2018-09-23 23:04:10 -0500

    wrote media mention model

--- Storage/src/Models/MediaMention.java
+++ Storage/src/Models/MediaMention.java
@@ -1,8 +1,63 @@
 package Models;
 
+import java.sql.Timestamp;
+
 /**
- * Created by lucyfox on 9/23/18.
+ * Created by lucy and ameesh on 9/23/18.
  */
 public class MediaMention {
-    
+
+    enum MediaType {
+        WEB, NEWSPAPER, RADIO, TV
+    }
+
+    enum Scope {
+        NATIONAL, INTERNATIONAL, LOCAL
+    }
+
+    enum Category {
+        FEATURED, NATIONAL_INTERNATIONAL, HOUSTON_TEXAS,
+        BROADCAST, TRADE_PROFESSONAL, OTHER_NEWS, SPORTS
+    }
+
+    public MediaMention(
+        String headline,
+        String outletName,
+        MediaType mediaType,
+        Scope scope,
+        String locale,
+        String language,
+        Timestamp coverageDate,
+        Timestamp datelineDate,
+        Category category
+    ) throws Exception {
+        this.headline = headline;
+        this.outletName = outletName;
+        this.mediaType = mediaType;
+        this.scope = scope;
+        this.locale = locale;
+        this.language = language;
+        this.coverageDate = coverageDate;
+        this.datelineDate = datelineDate;
+        this.category = category;
+    }
+
+    private String headline;
+
+    private String outletName;
+
+    private MediaType mediaType;
+
+    private Scope scope;
+
+    private String locale;
+
+    private String language;
+
+    private Timestamp coverageDate;
+
+    private Timestamp datelineDate;
+
+    private Category category;
+
 }
